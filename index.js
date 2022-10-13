f29eff9089bcb3c7c6f209f6d4e2585c425cd324
Jiakan Wang <jkw@statsig.io>
2022-10-12 22:51:02 -0700

    first version of connector

---
+++ index.js
@@ -0,0 +1,66 @@
+const statsigMetadata = {
+    sdkType: "statsig-unleash-connector",
+    sdkVersion: "0.1.0",
+}
+
+const statsigConnector = {
+    initialize: function(clientKey) {
+        statsigConnector._clientKey = clientKey;
+        statsigConnector._dedupeKeys = {};
+    },
+
+    logExposure: function(toggle, userID, enabled) {
+        const dedupeKey = `${toggle}:${userID}:${enabled}`;
+        if (statsigConnector._dedupeKeys[dedupeKey]) {
+            return;
+        } else {
+            statsigConnector._dedupeKeys[dedupeKey] = true;
+        }
+
+        const body = {
+            events: [
+                {
+                    eventName: "statsig::gate_exposure",
+                    time: Date.now(),
+                    user: { userID },
+                    metadata: {
+                        gate: toggle,
+                        gateValue: String(enabled),
+                    }
+                }
+            ],
+            statsigMetadata: statsigMetadata,
+        };
+
+        logEvent(body);
+    },
+
+    logEvent: function() {
+        const body = {
+            events: [
+                {
+                eventName,
+                time: Date.now(),
+                user: { userID }
+                }
+            ],
+            statsigMetadata: statsigMetadata,
+        };
+        logEvent(body);
+    },
+
+    _logEvent: async function(body) {
+        const params = {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "STATSIG-API-KEY": statsigConnector._clientKey,
+                "STATSIG-CLIENT-TIME": Date.now() + ""
+            }
+        };
+        await fetch("https://events.statsigapi.net/v1/rgstr", params);
+    }
+}
+
+export default statsigConnector;
